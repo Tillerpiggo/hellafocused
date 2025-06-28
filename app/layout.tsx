cd06beb0ce0009e62eeefe37022f50c7143af0fd
Tillerpiggo <36749446+Tillerpiggo@users.noreply.github.com>
2025-06-28 15:45:08 -0700

    Remove unnecessary/empty themeProvider wrapper and use themeProvider directly instead

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next"
 import { Outfit } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "next-themes"
 
 const fontSans = Outfit({
   subsets: ["latin"],
